7d346e7682af648937bc60a957af3b2969ad217c
AliNajimov <najimovm11@gmail.com>
2023-09-13 16:52:29 +0500

    done

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,18 @@
 package com.example.listener;
 
-public class ContextListener {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        LocalDateTime servletTimeInit = LocalDateTime.now();
+        context.setAttribute("servletTimeInit", servletTimeInit);
+    }
     //write your code here!
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,36 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession httpSession = req.getSession(false);
+        if (httpSession == null || httpSession.getAttribute("user") == null) {
+            resp.sendRedirect("/login.jsp");
+        } else {
+            resp.sendRedirect("/user/hello.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String admin = "admin";
+        String user = "user";
+        String password = req.getParameter("password");
+        if (!password.isEmpty() && (admin.equals(req.getParameter("login")) || user.equals(req.getParameter("login")))) {
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("user", admin);
+            resp.sendRedirect("/user/hello.jsp");
+        }else {
+            resp.sendRedirect("/login.jsp");
+        }
+    }
 }
